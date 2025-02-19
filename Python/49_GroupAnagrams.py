0a87169c69d591c19a130b11e78ed8e5303b9e7f
RogerChou12 <yi.chen.chou.work@gmail.com>
2025-02-19 13:55:33 +0000

    49. Group Anagrams

---
+++ Python/49_GroupAnagrams.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_map=defaultdict(list)
+
+        for word in strs:
+            sort_word=''.join(sorted(word)) # list of characters convert to string
+            anagram_map[sort_word].append(word)
+        
+        return list(anagram_map.values())

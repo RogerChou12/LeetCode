11df3445d7d49a2ea83e0460df55f25c2393d3d4
RogerChou12 <yi.chen.chou.work@gmail.com>
2025-02-23 13:29:02 +0000

    152. Maximum Product Subarray

---
+++ C/152_MaximumProductSubarray.c
@@ -0,0 +1,14 @@
+int maxProduct(int* nums, int numsSize) {
+    int max_product=INT_MIN, prefix=1, suffix=1;
+
+    for(int i=0;i<numsSize;i++){
+        // product==0 then reset 1
+        if(prefix==0) prefix=1;
+        if(suffix==0) suffix=1;
+        prefix *= nums[i];
+        suffix *= nums[numsSize-1-i];
+        max_product = fmax(max_product, fmax(prefix, suffix));
+    }
+
+    return max_product;
+}

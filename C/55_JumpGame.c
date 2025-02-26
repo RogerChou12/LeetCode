a417ef39fc152f0fd0d67accd023d882589ea1fe
RogerChou12 <yi.chen.chou.work@gmail.com>
2025-02-26 02:16:37 +0000

    55. Jump Game

---
+++ C/55_JumpGame.c
@@ -0,0 +1,9 @@
+bool canJump(int* nums, int numsSize) {
+    int furthest=0;
+    // keep track the furthest reachable index
+    for(int i=0;i<numsSize;i++){
+        if(furthest < i) return false;
+        furthest = fmax(furthest, i+nums[i]);
+    }
+    return true;
+}

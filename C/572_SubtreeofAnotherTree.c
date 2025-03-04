eaf1396515b0f300380330e492c5bda6ca17ecbb
RogerChou12 <yi.chen.chou.work@gmail.com>
2025-03-04 02:28:16 +0000

    572. Subtree of Another Tree

---
+++ C/572_SubtreeofAnotherTree.c
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool isSame(struct TreeNode* root, struct TreeNode* subRoot){
+    if(!root && !subRoot) return true;
+    else if(!root || !subRoot || root->val != subRoot->val) return false;
+    return root->val==subRoot->val && isSame(root->left, subRoot->left) && isSame(root->right, subRoot->right);
+}
+bool isSubtree(struct TreeNode* root, struct TreeNode* subRoot) {
+    if(!root) return false;
+    if(root->val == subRoot->val){
+        if(isSame(root, subRoot)) return true;
+    }
+    return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+}

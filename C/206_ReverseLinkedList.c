5984e96846328fc1af664b6cb28d8bf7f513f32b
RogerChou12 <yi.chen.chou.work@gmail.com>
2025-03-07 03:00:00 +0000

    206. Reverse Linked List

---
+++ C/206_ReverseLinkedList.c
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode *previous, *reverse=NULL;
+    while(head){
+        previous = head->next;
+        head->next = reverse;
+        reverse = head;
+        head = previous; // move to next node
+    }
+    return reverse;
+}

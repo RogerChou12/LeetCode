80b97fe14c1a983b3f228f2fb4b229a9cca3f7e8
RogerChou12 <yi.chen.chou.work@gmail.com>
2025-03-14 09:07:12 +0000

    91. Decode Ways

---
+++ C/91_DecodeWays.C
@@ -0,0 +1,13 @@
+int numDecodings(char* s) {
+    int now=1, pre=0; // now=decoding ways from 'i+1', pre=decoding ways from 'i+2'
+    int len=strlen(s);
+    for(int i=len-1;i>=0;i--){
+        int cur=(s[i]=='0')?0:now; // Single Digit Decoding, 0 cannot be decoded
+        if(i<len-1 && (s[i]=='1' || s[i]=='2' && s[i+1]<'7')){
+            cur += pre; // Two-Digit Decoding
+        }
+        pre=now;
+        now=cur;
+    }
+    return now;
+}

9e20e17ef862a5e07acb4b61a436f485a4abb5a8
RogerChou12 <yi.chen.chou.work@gmail.com>
2025-03-17 02:52:37 +0000

    98. Validate Binary Search Tree

---
+++ C/98_ValidateBinarySearchTree.c
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool validate(struct TreeNode *root, long max, long min){
+    if(!root) return true; // valid BST
+    if(root->val<=min || root->val>=max) return false;
+    return validate(root->left, root->val, min) && validate(root->right, max, root->val);
+}
+bool isValidBST(struct TreeNode* root) {
+    return validate(root, LONG_MAX, LONG_MIN);
+}

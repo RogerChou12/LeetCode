819bcf11cbce282a16174c8782a81ba701d758c2
RogerChou12 <yi.chen.chou.work@gmail.com>
2025-03-18 02:31:57 +0000

    121. Best Time to Buy and Sell Stock

---
+++ C/121_BestTimetoBuyandSellStock.c
@@ -0,0 +1,10 @@
+int maxProfit(int* prices, int pricesSize) {
+    int min=prices[0], profit=0;
+
+    for(int i=0;i<pricesSize;i++){
+        profit = fmax(profit, (prices[i]-min));
+        min = fmin(min, prices[i]);
+    }
+
+    return profit;
+}

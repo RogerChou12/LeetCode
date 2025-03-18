50dfe1bed45e5a19816cb66176c1d16b84518724
RogerChou12 <yi.chen.chou.work@gmail.com>
2025-03-18 03:07:15 +0000

    125. Valid Palindrome

---
+++ C/125_ValidPalindrome.c
@@ -0,0 +1,18 @@
+#include<stdbool.h>
+
+bool isPalindrome(char* s) {
+    int left=0, right=strlen(s)-1;
+
+    while(left < right){
+        // skip non-alphanumeric characters
+        while(left<right && !isalnum(s[left])) left++;
+        while(left<right && !isalnum(s[right])) right--;
+
+        if(tolower(s[left]) != tolower(s[right])) return false;
+
+        left++;
+        right--;
+    }
+
+    return true;
+}

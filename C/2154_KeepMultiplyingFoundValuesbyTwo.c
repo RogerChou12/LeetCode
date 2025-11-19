fd859fdd571faf2a00fc0119eefab3f82010aa23
RogerChou12 <yi.chen.chou.work@gmail.com>
2025-11-19 02:26:51 +0000

    2154. Keep Multiplying Found Values by Two

---
+++ C/2154_KeepMultiplyingFoundValuesbyTwo.c
@@ -0,0 +1,13 @@
+int cmpInt(const void *a, const void *b){
+    return *(int*)a-*(int*)b;
+}
+
+int findFinalValue(int* nums, int numsSize, int original) {
+    qsort(nums, numsSize, sizeof(int), cmpInt);
+    for(int i=0;i<numsSize;i++){
+        if(nums[i]==original){
+            original=original<<1;
+        }
+    }
+    return original;
+}
